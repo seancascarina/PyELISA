7f4243b063b878d0c0a6480a75b87c56e512c117
Sean Cascarina <31107448+seancascarina@users.noreply.github.com>
2025-07-16 16:20:25 -0600

    Add input validation function

--- PyELISA.py
+++ PyELISA.py
@@ -3,7 +3,30 @@ from scipy.optimize import curve_fit
 
 def main(args):
 
+    data_file = args.data_file
+    file_type = args.file_type.lower()
     
+    validate_input(data_file, file_type)
+    
+    
+def validate_input(data_file, file_type):
+    
+    error_message = ''
+    # VALIDATE FILE
+    try:
+        h = open(data_file)
+        h.close()
+    except:
+        error_message += '\nERROR: The file could not be found. Please ensure the file is in the same directory as PyELISA or specify an absolute file path.\n'
+        
+    if file_type not in ['csv', 'tsv']:
+        error_message += '\nERROR: Unrecognized file type. Valid file types of "tsv" or "csv"'
+        
+    if error_message:
+        print(error_message)
+        print('\nExiting prematurely...')
+        exit()
+
 
 def get_args(arguments):
     parser = argparse.ArgumentParser(description='Calculate endpoint titer (ET) from ELISA data', prog='PyELISA')
