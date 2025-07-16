82803ee493dbfa118070b1d10eb82f8d51e1a97b
Sean Cascarina <31107448+seancascarina@users.noreply.github.com>
2025-07-16 16:11:17 -0600

    Add command-line args for specifying files

--- PyELISA.py
+++ PyELISA.py
@@ -1,7 +1,22 @@
 
 from scipy.optimize import curve_fit
 
-def main():
+def main(args):
+
+    
+
+def get_args(arguments):
+    parser = argparse.ArgumentParser(description='Calculate endpoint titer (ET) from ELISA data', prog='PyELISA')
+    
+    parser.add_argument('data_file', help="""Your data file.""")
+    parser.add_argument('file_type', default='tsv', help="""Your file type (csv or tsv). Default=tsv""")
+
+    args = parser.parse_args(arguments)
+    
+    return args
+
 
 if __name__ == '__main__':
-    main()
+    import sys, argparse
+    args = get_args(sys.argv[1:])
+    main(args)
