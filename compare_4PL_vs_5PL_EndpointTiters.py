8f28e0cfb3d106263ba61a81e24a4bed7413579f
Sean Cascarina <31107448+seancascarina@users.noreply.github.com>
2025-08-01 07:28:58 -0600

    Add get_endpoint_titers() function

--- compare_4PL_vs_5PL_EndpointTiters.py
+++ compare_4PL_vs_5PL_EndpointTiters.py
@@ -1,7 +1,16 @@
 
+import pandas as pd
 
 def main():
+    
+    get_endpoint_titers('Endpoint_Titer_Results_4PL.tsv')
+    
+def get_endpoint_titers(file):
+    
+    df = pd.read_csv(file, sep='\t')
 
+    return df
+    
 
 if __name__ == '__main__':
     main()
