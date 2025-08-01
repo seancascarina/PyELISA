8d2252714ebae731bc3271054e5b43eb3bd925b0
Sean Cascarina <31107448+seancascarina@users.noreply.github.com>
2025-08-01 07:44:23 -0600

    Add initial plotting() function

--- compare_4PL_vs_5PL_EndpointTiters.py
+++ compare_4PL_vs_5PL_EndpointTiters.py
@@ -1,5 +1,7 @@
 
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 def main():
     
@@ -10,10 +12,14 @@ def main():
     df_5pl.drop('g', axis=1, inplace=True)
     df_4pl['regression_type'] = '4PL'
     df_5pl['regression_type'] = '5PL'
-    df = pd.concat([df_4pl, df_5pl])
+    df = pd.concat([df_4pl, df_5pl], ignore_index=True)
     df['xtick_label'] = df['Group'] + ', ' + df['Sample ID']
     
     
+def plotting(df):
+    
+    sns.barplot(x='xtick_label', y='1 / Endpoint_Titer', data=df, hue='regression_type')
+    plt.show()
     
     
 def get_endpoint_titers(file):
